75ee28d7a6e609a756efb3f04844821ab75b300b
Victoria Stauffer <vstauffer21@gmail.com>
2020-09-11 13:43:34 -0400

    Added projects and art pages

--- wp-content/themes/portfolio/front-page.php
+++ wp-content/themes/portfolio/front-page.php
@@ -4,8 +4,8 @@
     <div class="center_content">
     <div class="body_head">Hello,</div>
     <div class="body_txt">
-            <p>My name is Victoria Stauffer. I am a junior Drexel University, enrolled in the Interactive Digital Media program.  So far my interests for this major lie in creative problem solving, research contributing to UX Design, and developing a strong skillset to develop websites.</p>
-            <p>Outside of this major I'm interested in environmentalism and sustainable living, and as a hobby I love to draw and paint. I have been practicing fine art since high school and it's one of the things that drew me to the IDM program! I have also started to really enjoy sewing. I love altering clothes and creating fun costumes.</p>
+            <p>My name is Victoria Stauffer. I am a junior at Drexel University, enrolled in the Interactive Digital Media program.  So far my interests for this major lie in creative problem solving, research contributing to UX Design, and developing a strong skillset to code websites.</p>
+            <p>Outside of this major, I'm interested in environmentalism and sustainable living, and as a hobby I love to draw and paint. I have been practicing fine art since high school and it's one of the things that drew me to the IDM program. I also enjoy sewing. I love altering clothes and creating fun costumes.</p>
             <p>Take a look around! All of my projects, from in and out of school are here on my site!  =☽</p>    
     </div>
     </div>
--- wp-content/themes/portfolio/single-art_posts.php
+++ wp-content/themes/portfolio/single-art_posts.php
@@ -9,6 +9,24 @@
         
         <h1 class="art_detail_title"><?php echo get_field('adp_title') ;?></h1>
         <p class="art_detail_description"><?php echo get_field('adp_description'); ?></p>
+        <?php 
+        $link = get_field('adp_link');
+        if( $link ): 
+            $link_url = $link['url'];
+            $link_title = $link['title'];
+            $link_target = $link['target'] ? $link['target'] : '_self';
+            ?>
+            <a class="button art_detail_link" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+        <?php endif; ?>
+        <?php 
+        $link = get_field('adp_link_2');
+        if( $link ): 
+            $link_url = $link['url'];
+            $link_title = $link['title'];
+            $link_target = $link['target'] ? $link['target'] : '_self';
+            ?>
+            <a class="button art_detail_link" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+        <?php endif; ?>
     </div>
     <div class="push"></div>
 </main>
